99fab9d5d1cc0e43da1421b969b0dd5587296b24
Lucas Dutra <ldutra98@gmail.com>
2025-08-19 23:38:36 -0300

    Remove sqlalchemy configs

--- project/config.py
+++ project/config.py
@@ -6,27 +6,27 @@ class BaseConfig:
     """Base configuration"""
 
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
 
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
 
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
+    """Production configuration"""
+
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class StageConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    """Staging configuration"""
+
+    DEBUG = False
